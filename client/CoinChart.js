7b29e4db8bdf6ebd1adb8c82ffee6c92c15e32e4
sidshank <siddharth.shankar@gmail.com>
2018-06-10 17:16:27 -0400

    Added comments in CoinChart.js

--- client/CoinChart.js
+++ client/CoinChart.js
@@ -3,6 +3,7 @@ import Chart from 'chart.js';
 export class CoinChart {
 
     constructor(context) {
+        this.MAX_DATA_POINTS = 25;
         this.chart = new Chart(context, {
             type: 'line',
             data: {
@@ -29,6 +30,14 @@ export class CoinChart {
                 }
             }
         });
+        this.resetChartData();
+    }
+
+    resetChartData() {
+        this.chart.data.labels = [];
+        this.chart.data.datasets.forEach((dataset) => {
+            dataset.data = [];
+        });
     }
 
     setDatasetLabel(datasetNumber, label) {
@@ -39,20 +48,25 @@ export class CoinChart {
 
     addData(label, value, l, h) {
         this.chart.data.labels.push(label);
-        if(this.chart.data.labels.length === 25) {
+        if(this.chart.data.labels.length > this.MAX_DATA_POINTS) {
             this.chart.data.labels.shift();
         }
+
         this.chart.data.datasets.forEach((dataset) => {
             dataset.data.push(value);
-            if (dataset.data.length === 26) {
+            if (dataset.data.length > this.MAX_DATA_POINTS) {
                 dataset.data.shift();
             }
         });
+
+        // When we get the first data point, we scale the chart. Since this is
+        // just a toy / demo app, the scale picked here is somewhat arbitrary
+        // and is meant to show even small deviations in the incoming data.
         if (0 === this.chart.options.scales.yAxes[0].ticks.suggestedMin) {
             this.chart.options.scales.yAxes[0].ticks.suggestedMin = 0.995*value;
             this.chart.options.scales.yAxes[0].ticks.min = 0.995*value;
         }
-        if (1 ===  this.chart.options.scales.yAxes[0].ticks.suggestedMax) {
+        if (1 === this.chart.options.scales.yAxes[0].ticks.suggestedMax) {
             this.chart.options.scales.yAxes[0].ticks.suggestedMax = 1.005*value;
             this.chart.options.scales.yAxes[0].ticks.max = 1.005*value;
         }
@@ -61,10 +75,7 @@ export class CoinChart {
     }
     
     removeData() {
-        this.chart.data.labels = [];
-        this.chart.data.datasets.forEach((dataset) => {
-            dataset.data = [];
-        });
+        this.resetChartData();
         this.chart.options.scales.yAxes[0].ticks.suggestedMin = 0;
         this.chart.options.scales.yAxes[0].ticks.min = 0;
         this.chart.options.scales.yAxes[0].ticks.suggestedMax = 1;
